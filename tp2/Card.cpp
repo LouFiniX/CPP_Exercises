6aaf26605a6e0e7ce5463a0393f06bcc6c9b3ee7
Fily Nino <LouFiniX@outlook.fr>
2024-01-29 14:13:51 +0100

    tp2 - card -q5

---
+++ tp2/Card.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <iostream>
+#include "Card.hpp"
+
+Card::Card(unsigned int value, std::string color)
+    : _value {value}
+    ,_color {color}
+    {}
+
+void Card::print(){
+    std::cout << _value << " de " << _color << std::endl;
+}
